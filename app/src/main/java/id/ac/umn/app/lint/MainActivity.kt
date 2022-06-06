2c10670ce5d3d2bf802ed8820a586a76d597141d
Gerardo Jason <gerardoojason@gmail.com>
2022-06-06 21:39:24 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/umn/app/lint/MainActivity.kt
@@ -0,0 +1,17 @@
+package id.ac.umn.app.lint
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+//class MainActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//    }
+//    @SuppressLint("NewApi")
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.main)
+//
+//    }
